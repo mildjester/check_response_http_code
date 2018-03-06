8abd849d63c67636e617c7d54e2f4801dec1e26c
mildjester <16119464+mildjester@users.noreply.github.com>
2018-03-07 00:02:44 +0900

    create repository

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # check_response_http_code
+
+## Discription
+
+This script checks a reply HTTP cord of the target URL.
+Target URL list must be written separated by newline.
+
+## Usage
+
+```
+./check_response_http_code {TargetUrlList}
+```
+
+## Example
+```
+./check_response_http_code target-file-example.txt
+```
+
---
+++ check_response_http_code.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! $# -eq 1 ]; then
+    echo "Usage: ./check_response_http_code.sh target-file.txt"
+    exit
+fi
+
+if [ ! -e $1 ]; then
+    echo "Target File is Not Found. (" $1 ")"
+    exit
+fi
+
+TARGET_FILE=$1
+CUR_DATE=`date "+%Y%m%d%H%M%S"`
+
+getHttpCd () {
+  result=`curl -v $1 2>&1`
+  resCd=`echo $result | grep -o "HTTP/2 [0-9]\{3\}" | grep -o "[0-9]\{3\}"`
+  if [ $redirect_cnt -gt 0 ]; then
+    for i in `seq $redirect_cnt`
+    do
+      printf "="
+    done
+    printf ">"
+  fi
+  echo "$resCd : $1"
+  if [ $resCd -eq 301 ] || [ $resCd -eq 302 ] ; then
+    redirect_cnt=`expr $redirect_cnt + 1`
+    next_url=`echo $result | grep -o "location: https://[0-9a-zA-Z\.-_/]*" | grep -o "https://[0-9a-zA-Z\.-_/]*" | xargs`
+    getHttpCd $next_url
+  fi
+}
+
+while read line
+do
+  redirect_cnt=0
+  getHttpCd $line
+  sleep 1s
+done < $TARGET_FILE
+
